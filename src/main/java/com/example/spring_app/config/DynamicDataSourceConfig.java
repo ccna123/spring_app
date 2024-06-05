bafc78aa877d3023bdc87d5e6ee3797abea887a4
THANH <95860402+ccna123@users.noreply.github.com>
2024-06-05 21:36:03 +0900

    update

--- src/main/java/com/example/spring_app/config/DynamicDataSourceConfig.java
+++ src/main/java/com/example/spring_app/config/DynamicDataSourceConfig.java
@@ -2,19 +2,16 @@ package com.example.spring_app.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
-import com.example.spring_app.controller.RestController.StudentController;
-
 import javax.sql.DataSource;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -27,63 +24,34 @@ public class DynamicDataSourceConfig {
 
     @Bean
     DataSource dataSource() {
+        File[] files = Paths.get("allTenants").toFile().listFiles();
+        Map<Object, Object> resolvedDataSources = new HashMap<>();
+
+        for (File propertyFile : files) {
+            Properties tenantProperties = new Properties();
+            @SuppressWarnings("rawtypes")
+            DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+
+            try {
+                tenantProperties.load(new FileInputStream(propertyFile));
+                String tenantId = tenantProperties.getProperty("name");
+
+                dataSourceBuilder.driverClassName(tenantProperties.getProperty("datasource.driver-class-name"));
+                dataSourceBuilder.username(tenantProperties.getProperty("datasource.username"));
+                dataSourceBuilder.password(tenantProperties.getProperty("datasource.password"));
+                dataSourceBuilder.url(tenantProperties.getProperty("datasource.url"));
+                resolvedDataSources.put(tenantId, dataSourceBuilder.build());
+            } catch (IOException exp) {
+                throw new RuntimeException("Problem in tenant datasource:" + exp);
+            }
+        }
+
         AbstractRoutingDataSource dataSource = new MultitenantDataSource();
-        dataSource.setDefaultTargetDataSource(createDefaultDataSource());
-        dataSource.setTargetDataSources(loadTenantDataSources());
+        dataSource.setDefaultTargetDataSource(resolvedDataSources.get(defaultTenant));
+        dataSource.setTargetDataSources(resolvedDataSources);
+
         dataSource.afterPropertiesSet();
         return dataSource;
-    }
 
-    private DataSource createDefaultDataSource() {
-        File defaultTenantFile = new File("allTenants", defaultTenant + ".properties");
-        if (!defaultTenantFile.exists()) {
-            throw new RuntimeException("Default tenant file not found: " + defaultTenantFile.getAbsolutePath());
-        }
-
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        Properties defaultTenantProps = loadProperties(defaultTenantFile);
-
-        dataSourceBuilder.driverClassName(defaultTenantProps.getProperty("datasource.driver-class-name"));
-        dataSourceBuilder.username(defaultTenantProps.getProperty("datasource.username"));
-        dataSourceBuilder.password(defaultTenantProps.getProperty("datasource.password"));
-        dataSourceBuilder.url(defaultTenantProps.getProperty("datasource.url"));
-
-        return dataSourceBuilder.build();
-    }
-
-    private Map<Object, Object> loadTenantDataSources() {
-        File[] tenantFiles = new File("allTenants").listFiles();
-        if (tenantFiles == null || tenantFiles.length == 0) {
-            throw new RuntimeException("No tenant files found in allTenants folder.");
-        }
-
-        Map<Object, Object> dataSources = new HashMap<>();
-        for (File tenantFile : tenantFiles) {
-            if (!tenantFile.isFile()) {
-                continue;
-            }
-            Properties tenantProps = loadProperties(tenantFile);
-
-            DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-            dataSourceBuilder.driverClassName(tenantProps.getProperty("datasource.driver-class-name"));
-            dataSourceBuilder.username(tenantProps.getProperty("datasource.username"));
-            dataSourceBuilder.password(tenantProps.getProperty("datasource.password"));
-            dataSourceBuilder.url(tenantProps.getProperty("datasource.url"));
-
-            String tenantName = tenantProps.getProperty("name");
-            dataSources.put(tenantName, dataSourceBuilder.build());
-        }
-
-        return dataSources;
-    }
-
-    private Properties loadProperties(File file) {
-        try (FileInputStream inputStream = new FileInputStream(file)) {
-            Properties properties = new Properties();
-            properties.load(inputStream);
-            return properties;
-        } catch (IOException e) {
-            throw new RuntimeException("Error loading properties file: " + file.getAbsolutePath(), e);
-        }
     }
 }
